2f11b683d7dd542c753b8d6daa1c4ab716b1002f
wu.kai <wu.kai@navercorp.com>
2021-11-12 18:08:19 +0800

    save

--- js/index.js
+++ js/index.js
@@ -1,9 +1,5 @@
 
 const { host, protocol, hostname } = location;
-console.log(hostname);
-console.log(host);
-console.log(protocol);
-console.log(`${protocol}//${host}/wu-kai.github.io/docs/test/index.md`);
 const localDocsUrl = `${protocol}//${host}/wu-kai.github.io/`;
 const docsUrl = hostname === 'localhost' ? localDocsUrl : '/';
 $('document').ready(function () {
