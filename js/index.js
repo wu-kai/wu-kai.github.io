0ff9f8c33b61b9b03c85df11e55bcd9b82697f1a
wu.kai <wu.kai@navercorp.com>
2021-11-12 17:48:32 +0800

    测试

--- js/index.js
+++ js/index.js
@@ -1,7 +1,15 @@
 
+const { host, protocol, hostname } = location;
+console.log(hostname);
+console.log(host);
+console.log(protocol);
+console.log(`${protocol}//${host}/wu-kai.github.io/docs/test/index.md`);
+const localDocsUrl = `${protocol}//${host}/wu-kai.github.io/`;
+const docsUrl = hostname === 'localhost' ? localDocsUrl : '/';
 $('document').ready(function () {
   console.log(marked );
-  $.get('../docs/test/index.md', function (d) {
+  $.get(`${docsUrl}docs/test/index.md`, function (d) {
+    console.log(d);
     document.getElementById('content').innerHTML =
       marked.parse(d);
   })
